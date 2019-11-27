069531f84c218483aa2d4339ef22a872974231ed
Axel Ekwall <git@axelekwall.se>
2019-11-27 14:53:29 +0100

    clicking things works

---
+++ BurnForestBurn/Assets/Networking/ClickHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Obsolete]
+public class ClickHandler : NetworkBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit rayhit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out rayhit))
+            {
+                GameObject hitObject = rayhit.collider.gameObject;
+
+                if (hitObject.tag == "tree")
+                {
+                    CmdDestroyTree(hitObject);
+                }
+            }
+        }
+    }
+
+    [Command]
+    void CmdDestroyTree(GameObject tree)
+    {
+        Debug.Log("HIT");
+        NetworkServer.Destroy(tree);
+    }
+}
